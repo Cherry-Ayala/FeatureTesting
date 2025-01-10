b1af9e6ef6ba67dd3cc13e58ad8f505c35ad8c6c
Cherry-Ayala <cherry.devservices@gmail.com>
2025-01-10 14:49:01 -0600

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,30 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+print(os.environ.get("HF_URL"))
+print(os.environ.get("HF_API_KEY"))
+
+client = OpenAI(
+ base_url=os.environ["HF_URL"],
+ api_key=os.environ["HF_API_KEY"]
+)
+
+messages = [
+ {
+  "role": "user",
+  "content": "What is the capital of France?"
+ }
+]
+
+stream = client.chat.completions.create(
+    model="microsoft/phi-4", 
+ messages=messages, 
+ max_tokens=500,
+ stream=True
+)
+
+for chunk in stream:
+    print(chunk.choices[0].delta.content, end="")
